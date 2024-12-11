66d74360b38f6d0f10653c91af8d50f206463c11
Rasyid Firmansyah <rasyidfirmansyah56@student.uns.ac.id>
2024-12-11 14:53:24 +0700

    refactor: simplify code

--- src/main/java/com/pbo/marketplace/writer/DatabaseWriter.java
+++ src/main/java/com/pbo/marketplace/writer/DatabaseWriter.java
@@ -2,38 +2,29 @@ package com.pbo.marketplace.writer;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.pbo.marketplace.domain.entities.Cart;
-import com.pbo.marketplace.retrieve.DatabaseRetrieve;
 
 public class DatabaseWriter {
-    public <T> void JSONWriter(T clazz, String path) {
-        GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
+    public DatabaseWriter(List<?> clazzList, String path) {
+        this.JSONWriter(clazzList, path);
+    }
 
-        Gson gson = builder.create();
+    private <T> void JSONWriter(List<T> clazzList, String path) {
+        new Thread(() -> {
+            GsonBuilder builder = new GsonBuilder();
+            builder.setPrettyPrinting();
 
-        DatabaseRetrieve databaseRetriever = new DatabaseRetrieve();
-        List<Cart> cartList = new ArrayList<>();
+            Gson gson = builder.create();
 
-        cartList = databaseRetriever.JSONParser(Cart.class, "src/main/java/com/pbo/marketplace/database/carts.json");
-
-        // If the file is empty, create a new list again
-        if (cartList == null) {
-            cartList = new ArrayList<>();
-        }
-
-        cartList.add((Cart) clazz);
-
-        String json = gson.toJson(cartList);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
-            writer.write(json);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+            String json = gson.toJson(clazzList);
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+                writer.write(json);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }).start();
     }
 }
